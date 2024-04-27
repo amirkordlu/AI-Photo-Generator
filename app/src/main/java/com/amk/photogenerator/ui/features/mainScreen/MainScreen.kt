fef8c1173e03f4e264720f322d5fb9241eba78a0
Amirmohammad Kordlu <amir.kordlu@gmail.com>
2024-04-27 22:24:43 +0330

    Main Toolbar added

--- app/src/main/java/com/amk/photogenerator/ui/features/mainScreen/MainScreen.kt
+++ app/src/main/java/com/amk/photogenerator/ui/features/mainScreen/MainScreen.kt
@@ -1,11 +1,25 @@
 package com.amk.photogenerator.ui.features.mainScreen
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.amk.photogenerator.R
 import com.amk.photogenerator.ui.theme.PhotoGeneratorTheme
 
 @Preview(showBackground = true)
@@ -23,4 +37,39 @@ fun MainScreenPreview() {
 @Composable
 fun MainScreen() {
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        MainToolbar()
+    }
+
+}
+
+@Composable
+fun MainToolbar() {
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(R.drawable.main_rectangle), contentDescription = null
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, start = 16.dp)
+        ) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Image(
+                    painter = painterResource(R.drawable.ic_account),
+                    contentDescription = "Account"
+                )
+            }
+        }
+
+    }
 }
